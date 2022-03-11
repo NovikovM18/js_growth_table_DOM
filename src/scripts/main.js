5e91823d8f0bdeeaaccb26f04676a921acff3a89
Misha Novikov <novikov1807@gmail.com>
2022-03-11 23:48:48 +0200

    not all...

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,103 @@
 'use strict';
 
-// write code here
+const tab = document.querySelector('tbody');
+const textTab = tab.innerHTML;
+const textArr = textTab.split('<tr>');
+
+textArr.shift();
+
+let newText = [];
+
+// function addCol() {
+//   newText = textArr.map(el => (`<tr> <td></td>` + el));
+
+//   const text = newText.join('');
+
+//   tab.innerHTML = text;
+// }
+
+// function reCol() {
+//   newText = textArr.map(el => (el.replace(`<td></td>`, ` `)));
+
+//   const text = newText.join('');
+
+//   tab.innerHTML = text;
+// }
+
+// function addRow() {
+//   const re = `<td></td>`;
+//   let rRe = ``;
+
+//   for (let i = 0; i < 4; i++) {
+//     rRe += re;
+//   }
+
+//   const row = `
+//   <tr>
+//     ${rRe}
+//   </tr>
+// `;
+
+//   textArr.push(row);
+
+//   const text = textArr.join('');
+
+//   tab.innerHTML = text;
+// }
+
+// function reRow() {
+//   textArr.pop();
+
+//   const text = textArr.join('');
+
+//   tab.innerHTML = text;
+// }
+
+document.querySelector('.append-row')
+  .addEventListener('click', () => {
+    const re = `<td></td>`;
+    let rRe = ``;
+
+    for (let i = 0; i < 4; i++) {
+      rRe += re;
+    }
+
+    const row = `
+    <tr>
+      ${rRe}
+    </tr>
+  `;
+
+    textArr.push(row);
+
+    const text = textArr.join('');
+
+    tab.innerHTML = text;
+  });
+
+document.querySelector('.remove-row')
+  .addEventListener('click', () => {
+    textArr.pop();
+
+    const text = textArr.join('');
+
+    tab.innerHTML = text;
+  });
+
+document.querySelector('.append-column')
+  .addEventListener('click', () => {
+    newText = textArr.map(el => (`<tr> <td></td>` + el));
+
+    const text = newText.join('');
+
+    tab.innerHTML = text;
+  });
+
+document.querySelector('.remove-column')
+  .addEventListener('click', () => {
+    newText = textArr.map(el => (el.replace(`<td></td>`, ` `)));
+
+    const text = newText.join('');
+
+    tab.innerHTML = text;
+  });
